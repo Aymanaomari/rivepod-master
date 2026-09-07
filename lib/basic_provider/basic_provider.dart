f60bd7d0c6707f09e54493352a92374871cd5c45
ayman aomari <167037493+Aymanaomari@users.noreply.github.com>
2026-09-07 23:49:56 +0100

    provider

---
+++ lib/basic_provider/basic_provider.dart
@@ -0,0 +1,23 @@
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'basic_provider.g.dart';
+
+@Riverpod(keepAlive: true)
+String basicProvider(BasicProviderRef ref) {
+  return "Hello Riverpod";
+}
+
+// Auto Dispose Provider
+@Riverpod(keepAlive: false)
+String autoDisProvider(AutoDisProviderRef ref) {
+  ref.onDispose(() {
+    print("AutoDisProvider has been disposed");
+  });
+  return "Hello AutoDispose Riverpod";
+}
+
+/// Family Provider
+@Riverpod(keepAlive: true)
+String familyProvider(FamilyProviderRef ref, {required String name}) {
+  return "Hello $name";
+}
